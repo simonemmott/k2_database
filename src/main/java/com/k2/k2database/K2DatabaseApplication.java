282e9aa4eb58324fd9764472579938b7996722e6
simon <simon.emmott@yahoo.co.uk>
2019-12-09 19:10:40 +0000

    Added spring application

---
+++ src/main/java/com/k2/k2database/K2DatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.k2.k2database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K2DatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K2DatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/k2/k2database/K2DatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k2.k2database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K2DatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
